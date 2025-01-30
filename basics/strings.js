edffc66cd7708bdd387c0e5ad592d891328a249b
Nagraj <ntadingi5@gmail.com>
2025-01-30 14:49:53 +0530

    strings and variables

---
+++ basics/strings.js
@@ -0,0 +1,9 @@
+const firstName = "Nagraj";
+const lastName = "Tadingi";
+console.log(firstName + " " + lastName);
+
+
+const city = "Rayagada";
+const country = "India";
+const location = city + ', ' + country;
+console.log(location)
