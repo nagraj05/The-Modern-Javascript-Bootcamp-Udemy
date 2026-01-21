6cfc391fc8533805948f539e26406016749ae931
Nagraj <ntadingi5@gmail.com>
2026-01-21 09:20:33 +0530

    learnt HTTP Requests

--- express-js/Express Server/index.js
+++ express-js/Express Server/index.js
@@ -3,6 +3,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  //   console.log(req.rawHeaders);
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login Pagettt");
+});
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
