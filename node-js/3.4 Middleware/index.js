fea475125720d4b6675faea02bfa68401cf79a0d
Nagraj <ntadingi5@gmail.com>
2026-01-21 10:52:29 +0530

    learnt custom middlewares

---
+++ node-js/3.4 Middleware/index.js
@@ -0,0 +1,16 @@
+import morgan from "morgan";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("short"));
+
+
+app.get("/", (req, res)=> {
+    res.send("Hello");
+})
+
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`);
+})
